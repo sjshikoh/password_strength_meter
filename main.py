79ea6e329bd25ad5ee437b08735bc4b61b7b4609
sjshikoh <148519226+sjshikoh@users.noreply.github.com>
2025-05-26 11:52:50 +0500

    installed streamlit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import string
 import re
 
 def check_password_strength(password):
@@ -8,4 +10,27 @@ def check_password_strength(password):
     else:
         print("Password should be at least 8 characters long.")
 
-    if 
+    if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
+        score += 1
+    else:
+        print("Include both upper and lower case letters.")
+
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        print("Add at least one digit from (0-9)")
+
+    if re.search(r"[!@#$%^&*]", password):
+        score += 1
+    else:
+        print("Include at least one special character (!@#$%^&*)")
+
+    if score == 4:
+        print("✅ Strong Password")
+    elif score == 3:
+        print("⚠️ Moderate Password")
+    else:
+        print("Weak Password")
+
+password = input("Enter your password: ")
+check_password_strength(password)
